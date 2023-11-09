889ec556c3220f2178b0d0da8794e29b17e36d24
Daniel Clavijo <d.clavijog@uniandes.edu.co>
2023-11-09 14:40:58 -0500

    Cambios

---
+++ docs/SQL/CargaMasiva.sql
@@ -0,0 +1,228 @@
+--Planes:
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 10.0, 1);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 15.0, 2);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 5.0, 3);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 20.0, 4);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 8.0, 5);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 10.0, 6);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 15.0, 7);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 20.0, 8);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 25.0, 9);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 30.0, 10);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 12.0, 11);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 18.0, 12);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 22.0, 13);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 35.0, 14);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 10.0, 15);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 16.0, 16);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 28.0, 17);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 25.0, 18);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 15.0, 19);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 30.0, 20);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 35.0, 21);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 40.0, 22);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 23);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 24);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 25);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 35.0, 26);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 40.0, 27);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 40.0, 28);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 20.0, 29);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 25.0, 30);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 10.0, 31);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 5.0, 32);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 33);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 5.0, 34);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 30.0, 35);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 15.0, 36);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 10.0, 37);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 38);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 30.0, 39);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 5.0, 40);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 10.0, 41);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 42);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 43);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 44);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 45);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 46);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 10.0, 47);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 48);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 10.0, 49);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 50);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 51);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 15.0, 52);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 53);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 54);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 55);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 15.0, 56);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 10.0, 57);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 58);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 59);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 60);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 25.0, 61);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 25.0, 62);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 63);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 64);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 20.0, 65);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 25.0, 66);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 67);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 15.0, 68);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 10.0, 69);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 70);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 71);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 72);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 73);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 74);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 75);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 76);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 77);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 78);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 79);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 80);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 81);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 82);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 83);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 84);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 85);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 86);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 87);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 88);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 89);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 90);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 91);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 92);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 93);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 94);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 95);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 96);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 97);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 98);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 99);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 100);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 101);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 102);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 103);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 104);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 105);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 106);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 107);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 108);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 109);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 110);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 111);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 112);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 113);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 114);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 115);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 116);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 117);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 118);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 119);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 120);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 121);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 122);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 123);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 124);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 125);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 126);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 127);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 128);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 129);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 130);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 131);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 132);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 133);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 134);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 135);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 136);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 137);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 138);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 139);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 140);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 141);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 142);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 143);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 144);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 145);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 146);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 147);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 148);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 149);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 150);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 151);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 152);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 153);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 154);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 155);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 156);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 157);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 158);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 159);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 160);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 161);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 162);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 163);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 164);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 165);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 166);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 167);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 168);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 169);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 170);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 171);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 172);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 173);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 174);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 175);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 176);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 177);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 178);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 179);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 180);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 181);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 182);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 183);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 184);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 185);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 186);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 187);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 188);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 189);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 190);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 191);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 192);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 193);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 194);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 195);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 196);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Todo_Incluido', 30.0, 197);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Larga_Estadia', 15.0, 198);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Promo_Particular', 20.0, 199);
+INSERT INTO planes (tipoplan, descuento, idplan) VALUES ('Tiempo_Compartido', 25.0, 200);
+
+
+--Alojamientos:
+BEGIN
+  FOR i IN 1..10000 LOOP -- Cambia 10000 por la cantidad de filas que quieres generar.
+    INSERT INTO alojamientos (
+      activa, 
+      checkin, 
+      checkout, 
+      acompanantes, 
+      idplan, 
+      idalojamiento
+    ) VALUES (
+      CASE MOD(i, 2) WHEN 0 THEN 'SI' ELSE 'NO' END, -- Alterna entre 'SI' y 'NO'.
+      TO_DATE('2005-01-01', 'YYYY-MM-DD') + TRUNC(DBMS_RANDOM.VALUE(0, 365 * (EXTRACT(YEAR FROM SYSDATE) - 2005))), -- Genera una fecha aleatoria de checkin desde el año 2005 hasta la fecha actual.
+      TO_DATE('2005-01-01', 'YYYY-MM-DD') + TRUNC(DBMS_RANDOM.VALUE(365 * (EXTRACT(YEAR FROM SYSDATE) - 2004), 365 * (EXTRACT(YEAR FROM SYSDATE) - 2003))), -- Genera una fecha de checkout posterior al checkin.
+      TRUNC(DBMS_RANDOM.VALUE(0, 9)), -- Genera un número entero aleatorio de acompañantes entre 0 y 8.
+      MOD(i, 200) + 1, -- Asigna un idplan de manera cíclica entre 1 y 200.
+      i -- Asigna un idalojamiento incremental.
+    );
+    IF MOD(i, 1000) = 0 THEN -- Realiza un commit cada 1000 registros para evitar bloqueos y uso excesivo de memoria.
+      COMMIT;
+    END IF;
+  END LOOP;
+  COMMIT;
+END;
+/
