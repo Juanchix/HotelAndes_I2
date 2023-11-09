b74633086c6dc11ed4f5949d794ba0346a5a76a4
Daniel Clavijo <d.clavijog@uniandes.edu.co>
2023-11-09 15:55:51 -0500

    Commit final

--- docs/SQL/CargaMasiva.sql
+++ docs/SQL/CargaMasiva.sql
@@ -226,3 +226,299 @@ BEGIN
   COMMIT;
 END;
 /
+
+--Usuarios:
+DECLARE
+  v_nombreuser    VARCHAR2(64);
+  v_tipodocuser   VARCHAR2(32);
+  v_numdocuser    NUMBER;
+  v_correouser    VARCHAR2(64);
+  v_iduser        NUMBER;
+  v_idalojamiento NUMBER;
+BEGIN
+  FOR i IN 1..10000 LOOP -- Cambia 10000 por la cantidad de usuarios que desees insertar.
+    -- Genera un nombre de usuario aleatorio. Puede que necesites una función para esto o usar un conjunto fijo de nombres.
+    v_nombreuser := 'Usuario ' || TO_CHAR(i);
+    
+    -- Asigna un tipo de documento de usuario aleatorio.
+    v_tipodocuser := CASE MOD(i, 5)
+                         WHEN 0 THEN 'CC'
+                         WHEN 1 THEN 'CE'
+                         WHEN 2 THEN 'NIT'
+                         WHEN 3 THEN 'PASAPORTE'
+                         WHEN 4 THEN 'TI'
+                     END;
+    
+    -- Genera un número de documento de usuario aleatorio.
+    v_numdocuser := TRUNC(DBMS_RANDOM.VALUE(10000000, 99999999));
+    
+    -- Genera un correo electrónico de usuario aleatorio. Puede que necesites una función para esto.
+    v_correouser := 'usuario' || TO_CHAR(i) || '@email.com';
+    
+    -- Establece el ID de usuario y alojamiento.
+    v_iduser := i;
+    v_idalojamiento := i;
+    
+    INSERT INTO usuarios (
+      nombreuser,
+      tipodocuser,
+      numdocuser,
+      correouser,
+      iduser,
+      idalojamiento
+    ) VALUES (
+      v_nombreuser,
+      v_tipodocuser,
+      v_numdocuser,
+      v_correouser,
+      v_iduser,
+      v_idalojamiento
+    );
+    
+    -- Realiza un commit cada 1000 registros para evitar bloqueos y uso excesivo de memoria.
+    IF MOD(i, 1000) = 0 THEN
+      COMMIT;
+    END IF;
+  END LOOP;
+  COMMIT;
+END;
+/
+--Hoteles:
+INSERT INTO hoteles (nombrehotel, nithotel, idhotel) VALUES ('Hotel Oasis', 123456789, 1);
+INSERT INTO hoteles (nombrehotel, nithotel, idhotel) VALUES ('Marina Bay Sands', 987654321, 2);
+INSERT INTO hoteles (nombrehotel, nithotel, idhotel) VALUES ('The Plaza', 555666777, 3);
+INSERT INTO hoteles (nombrehotel, nithotel, idhotel) VALUES ('Waldorf Astoria', 333222111, 4);
+INSERT INTO hoteles (nombrehotel, nithotel, idhotel) VALUES ('Beverly Hills Hotel', 444555666, 5);
+INSERT INTO hoteles (nombrehotel, nithotel, idhotel) VALUES ('Hotel Pacifico', 100000006, 6);
+INSERT INTO hoteles (nombrehotel, nithotel, idhotel) VALUES ('Hotel Atlantico', 100000007, 7);
+INSERT INTO hoteles (nombrehotel, nithotel, idhotel) VALUES ('Hotel Andino', 100000008, 8);
+INSERT INTO hoteles (nombrehotel, nithotel, idhotel) VALUES ('Hotel Caribe', 100000009, 9);
+INSERT INTO hoteles (nombrehotel, nithotel, idhotel) VALUES ('Hotel Amazonas', 100000010, 10);
+INSERT INTO hoteles (nombrehotel, nithotel, idhotel) VALUES ('Hotel Sierra', 100000011, 11);
+INSERT INTO hoteles (nombrehotel, nithotel, idhotel) VALUES ('Hotel Urbano', 100000012, 12);
+INSERT INTO hoteles (nombrehotel, nithotel, idhotel) VALUES ('Hotel Colonial', 100000013, 13);
+INSERT INTO hoteles (nombrehotel, nithotel, idhotel) VALUES ('Hotel Imperial', 100000014, 14);
+INSERT INTO hoteles (nombrehotel, nithotel, idhotel) VALUES ('Hotel Majestic', 100000015, 15);
+INSERT INTO hoteles (nombrehotel, nithotel, idhotel) VALUES ('Hotel Irotama', 100000016, 16);
+INSERT INTO hoteles (nombrehotel, nithotel, idhotel) VALUES ('Hotel Panorama', 100000017, 17);
+INSERT INTO hoteles (nombrehotel, nithotel, idhotel) VALUES ('Hotel Riviera', 100000018, 18);
+INSERT INTO hoteles (nombrehotel, nithotel, idhotel) VALUES ('Hotel Solaris', 100000019, 19);
+INSERT INTO hoteles (nombrehotel, nithotel, idhotel) VALUES ('Hotel Cosmos', 100000020, 20);
+
+--Tipos:
+INSERT INTO tipos (tipo, capacidad, dotacion, idtipo) VALUES ('Doble', 2, '2 camas, baño privado, TV', 1);
+INSERT INTO tipos (tipo, capacidad, dotacion, idtipo) VALUES ('Familiar', 4, '4 camas, 2 baños privados, TV, cocina', 2);
+INSERT INTO tipos (tipo, capacidad, dotacion, idtipo) VALUES ('Sencilla', 1, '1 cama, baño privado, TV', 3);
+INSERT INTO tipos (tipo, capacidad, dotacion, idtipo) VALUES ('Suite', 2, '1 cama grande, jacuzzi, baño privado, TV, sala de estar', 4);
+INSERT INTO tipos (tipo, capacidad, dotacion, idtipo) VALUES ('Suite Presidencial', 2, '1 cama king, jacuzzi, baño privado, TV, sala de estar, vista al mar', 5);
+INSERT INTO tipos (tipo, capacidad, dotacion, idtipo) VALUES ('Doble', 2, 'Cama doble, baño privado, TV', 6);
+INSERT INTO tipos (tipo, capacidad, dotacion, idtipo) VALUES ('Familiar', 4, 'Dos camas dobles, baño privado, TV, sala de estar', 7);
+INSERT INTO tipos (tipo, capacidad, dotacion, idtipo) VALUES ('Sencilla', 1, 'Cama sencilla, baño privado, TV', 8);
+INSERT INTO tipos (tipo, capacidad, dotacion, idtipo) VALUES ('Suite', 2, 'Cama Queen, baño de lujo, minibar, TV', 9);
+INSERT INTO tipos (tipo, capacidad, dotacion, idtipo) VALUES ('Suite Presidencial', 2, 'Cama King, jacuzzi, sala de estar, oficina, vista al mar', 10);
+INSERT INTO tipos (tipo, capacidad, dotacion, idtipo) VALUES ('Doble', 2, 'Cama doble, baño privado, TV, vista al jardín', 11);
+INSERT INTO tipos (tipo, capacidad, dotacion, idtipo) VALUES ('Familiar', 4, 'Dos camas dobles, baño privado, balcón, TV', 12);
+INSERT INTO tipos (tipo, capacidad, dotacion, idtipo) VALUES ('Sencilla', 1, 'Cama sencilla, baño privado, ventana al patio', 13);
+INSERT INTO tipos (tipo, capacidad, dotacion, idtipo) VALUES ('Suite', 2, 'Cama Queen, baño con tina, minibar, área de trabajo', 14);
+INSERT INTO tipos (tipo, capacidad, dotacion, idtipo) VALUES ('Suite Presidencial', 2, 'Cama King, sala de estar amplia, comedor, terraza privada', 15);
+INSERT INTO tipos (tipo, capacidad, dotacion, idtipo) VALUES ('Doble', 2, 'Dos camas individuales, baño privado, armario amplio', 16);
+INSERT INTO tipos (tipo, capacidad, dotacion, idtipo) VALUES ('Familiar', 4, 'Cama doble y literas, baño privado, mesa de juegos', 17);
+INSERT INTO tipos (tipo, capacidad, dotacion, idtipo) VALUES ('Sencilla', 1, 'Cama individual, baño con ducha, escritorio', 18);
+INSERT INTO tipos (tipo, capacidad, dotacion, idtipo) VALUES ('Suite', 2, 'Cama matrimonial, baño con hidromasaje, sala de estar con sofá', 19);
+INSERT INTO tipos (tipo, capacidad, dotacion, idtipo) VALUES ('Suite Presidencial', 2, 'Cama tamaño imperial, vestidor, baño de mármol, servicio de mayordomo', 20);
+
+--Habitaciones:
+DECLARE
+  v_numhabitacion NUMBER;
+  v_disponible    VARCHAR2(2);
+  v_precionoche   NUMBER;
+  v_idhotel       NUMBER;
+  v_idtipo        NUMBER;
+  v_idalojamiento NUMBER;
+BEGIN
+  FOR i IN 1..10000 LOOP -- Cambia 10000 por la cantidad de filas que deseas insertar.
+    v_numhabitacion := i;
+    v_disponible    := CASE MOD(i, 2) WHEN 0 THEN 'SI' ELSE 'NO' END;
+    v_precionoche   := TRUNC(DBMS_RANDOM.VALUE(50000, 1500000), -3); -- Redondea al múltiplo de 1000 más cercano dentro del rango.
+    v_idhotel       := MOD(i-1, 20) + 1; -- Asigna un ID de hotel de manera cíclica entre 1 y 20.
+    v_idtipo        := MOD(i-1, 20) + 1; -- Asigna un ID de tipo de manera cíclica entre 1 y 20.
+    v_idalojamiento := i; -- Asume que idalojamiento se incrementa igual que idhabitacion.
+    
+    INSERT INTO habitaciones (
+      numhabitacion,
+      disponible,
+      precionoche,
+      idhotel,
+      idtipo,
+      idalojamiento,
+      idhabitacion
+    ) VALUES (
+      v_numhabitacion,
+      v_disponible,
+      v_precionoche,
+      v_idhotel,
+      v_idtipo,
+      v_idalojamiento,
+      i -- idhabitacion se incrementa automáticamente con el bucle.
+    );
+    
+    IF MOD(i, 1000) = 0 THEN
+      COMMIT; -- Realiza un commit cada 1000 registros para evitar bloqueos y uso excesivo de memoria.
+    END IF;
+  END LOOP;
+  COMMIT; -- Hace commit de la última transacción si es que no alcanzó el múltiplo de 1000.
+END;
+/
+
+--Reservas:
+DECLARE
+  v_horareserva DATE;
+  v_idreserva   NUMBER;
+  v_idcuenta    NUMBER;
+BEGIN
+  FOR i IN 1..10000 LOOP -- Cambia 10000 por la cantidad de filas que deseas insertar.
+    -- Genera una fecha y hora aleatoria, evitando las 24 horas.
+    v_horareserva := TO_DATE('2023-01-01', 'YYYY-MM-DD') + (i - 1) + DBMS_RANDOM.VALUE(0, 23) / 24;
+    v_idreserva   := i;
+    v_idcuenta    := i; -- Asume que idcuenta se incrementa igual que idreserva.
+    
+    INSERT INTO reservas (
+      horareserva,
+      idreserva,
+      idcuenta
+    ) VALUES (
+      v_horareserva,
+      v_idreserva,
+      v_idcuenta
+    );
+    
+    IF MOD(i, 1000) = 0 THEN
+      COMMIT; -- Realiza un commit cada 1000 registros para evitar bloqueos y uso excesivo de memoria.
+    END IF;
+  END LOOP;
+  COMMIT; -- Hace commit de la última transacción si es que no alcanzó el múltiplo de 1000.
+END;
+/
+-- Servicios: 
+DECLARE
+  v_horarioinicial DATE;
+  v_horariofinal   DATE;
+  v_costo          NUMBER;
+  v_cargado        VARCHAR2(2);
+  v_existe         VARCHAR2(2);
+  v_idservicio     NUMBER;
+  v_idreserva      NUMBER;
+BEGIN
+  FOR i IN 1..10000 LOOP -- Cambia 10000 por la cantidad de servicios que desees insertar.
+    v_horarioinicial := TO_DATE('2023-01-01 07:00', 'YYYY-MM-DD HH24:MI') + (i-1); -- Asume que el servicio comienza a las 7 AM y añade un día por cada iteración.
+    v_horariofinal   := v_horarioinicial + INTERVAL '16' HOUR; -- Asume que el servicio termina 16 horas después.
+    v_costo          := TRUNC(DBMS_RANDOM.VALUE(100000, 3000000), -3); -- Genera un costo aleatorio entre 100000 y 3000000, redondeado al múltiplo de 1000 más cercano.
+    v_cargado        := CASE MOD(i, 2) WHEN 0 THEN 'SI' ELSE 'NO' END; -- Alterna entre 'SI' y 'NO'.
+    v_existe         := 'SI'; -- Asume que todos los servicios existen.
+    v_idservicio     := i; -- Asume que idservicio se incrementa automáticamente con el bucle.
+    v_idreserva      := i; -- Asume que idreserva se incrementa automáticamente con el bucle.
+
+    INSERT INTO servicios (
+      horarioinicial,
+      horariofinal,
+      costo,
+      cargado,
+      existe,
+      idservicio,
+      idreserva
+    ) VALUES (
+      v_horarioinicial,
+      v_horariofinal,
+      v_costo,
+      v_cargado,
+      v_existe,
+      v_idservicio,
+      v_idreserva
+    );
+
+    IF MOD(i, 1000) = 0 THEN
+      COMMIT; -- Realiza un commit cada 1000 registros para evitar bloqueos y uso excesivo de memoria.
+    END IF;
+  END LOOP;
+  COMMIT; -- Hace commit de la última transacción si es que no alcanzó el múltiplo de 1000.
+END;
+--Bares:
+DECLARE
+  TYPE estilos_typ IS VARRAY(5) OF VARCHAR2(50);
+  estilos estilos_typ := estilos_typ('Frances', 'Italiano', 'Mediterraneo', 'Mexicano', 'Oriental');
+BEGIN
+  FOR i IN 1..10000 LOOP -- Cambia 10000 por la cantidad de bares que deseas insertar.
+    INSERT INTO bares (
+      estilo,
+      idservicio,
+      idbar
+    ) VALUES (
+      estilos(MOD(i, 5) + 1), -- Esto ciclará a través del arreglo de estilos.
+      i, -- idservicio incrementa de 1 en 1.
+      i  -- idbar coincide con idservicio.
+    );
+    
+    IF MOD(i, 1000) = 0 THEN
+      COMMIT; -- Realiza un commit cada 1000 registros para evitar bloqueos y uso excesivo de memoria.
+    END IF;
+  END LOOP;
+  COMMIT; -- Hace commit de la última transacción si es que no alcanzó el múltiplo de 1000.
+END;
+/
+--Conferencias:
+BEGIN
+  FOR i IN 1..9000 LOOP -- Cambia 1000 por la cantidad de conferencias que deseas insertar.
+    INSERT INTO conferencias (
+      capacidad,
+      fecha,
+      hora,
+      duracion,
+      idservicio,
+      idconferencia
+    ) VALUES (
+      TRUNC(DBMS_RANDOM.VALUE(10, 500)), -- Genera un número aleatorio para la capacidad entre 10 y 500.
+      TRUNC(SYSDATE) + DBMS_RANDOM.VALUE(-365, 365), -- Genera una fecha aleatoria aproximadamente entre un año atrás y un año en el futuro.
+      TO_DATE('09:00', 'HH24:MI') + (DBMS_RANDOM.VALUE(0, 23) / 24), -- Genera una hora aleatoria para el mismo día.
+      TRUNC(DBMS_RANDOM.VALUE(30, 480)), -- Duración aleatoria entre 30 minutos y 8 horas (480 minutos).
+      9000 + i, -- idservicio se inicia en 9001 y se incrementa de 1 en 1.
+      9000 + i  -- idconferencia coincide con idservicio.
+    );
+    IF MOD(i, 100) = 0 THEN
+      COMMIT; -- Realiza un commit cada 100 registros para evitar bloqueos y uso excesivo de memoria.
+    END IF;
+  END LOOP;
+  COMMIT; -- Hace commit de la última transacción si es que no alcanzó el múltiplo de 100.
+END;
+/
+--Gimnasios:
+DECLARE
+  v_capacidad  NUMBER;
+  v_maquinas   VARCHAR2(255);
+  v_idservicio NUMBER;
+  v_idgimnasio NUMBER;
+BEGIN
+  v_maquinas := 'Cintas de correr, bicicletas estáticas'; -- Lista fija de máquinas.
+  FOR i IN 18001..19000 LOOP -- Cambia 19000 por el valor de inicio + la cantidad de gimnasios que deseas insertar.
+    v_capacidad := TRUNC(DBMS_RANDOM.VALUE(10, 100)); -- Capacidad aleatoria entre 10 y 100.
+    v_idservicio := i;
+    v_idgimnasio := i;
+    
+    INSERT INTO gimnasios (
+      capacidad,
+      maquinas,
+      idservicio,
+      idgimnasio
+    ) VALUES (
+      v_capacidad,
+      v_maquinas,
+      v_idservicio,
+      v_idgimnasio
+    );
+    
+    IF MOD(i, 100) = 0 THEN
+      COMMIT; -- Realiza un commit cada 100 registros para evitar bloqueos y uso excesivo de memoria.
+    END IF;
+  END LOOP;
+  COMMIT; -- Hace commit de la última transacción si es que no alcanzó el múltiplo de 100.
+END;
+/
